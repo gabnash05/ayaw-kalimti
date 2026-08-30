ccd628c330ed231169af4df95a6a1e4a6f60dd36
KIM GABRIEL NASAYAO <nasayaokim@gmail.com>
2026-08-30 12:24:20 +0800

    test(providers): add deterministic provider fakes

--- apps/api/jest.config.cjs
+++ apps/api/jest.config.cjs
@@ -1,8 +1,12 @@
 module.exports = {
   displayName: 'api',
   rootDir: __dirname,
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testPathIgnorePatterns: ['\\.preview\\.spec\\.ts$'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }],
   },
 };
